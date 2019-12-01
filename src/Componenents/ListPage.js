f9340d29286b9957d5a49f09acf814fb40ee0adf
Alexg78bis <alexg78bis@gmail.com>
2019-12-02 00:05:17 +0100

    favorite

--- src/Componenents/ListPage.js
+++ src/Componenents/ListPage.js
@@ -1,30 +1,38 @@
-import React from 'react'
-import { FlatList, View } from 'react-native'
-import { connect } from 'react-redux'
-import ListItem from './Exercices/ListItem'
+import React from "react";
+import { FlatList, View } from "react-native";
+import { connect } from "react-redux";
+import ListItem from "./ListItem";
 
 class ListPage extends React.Component {
   static navigationOptions = {
-    title: 'Liste des stations'
-  }
+    title: "Liste des stations"
+  };
 
-  render () {
+  render() {
     return (
       <View style={{ flex: 1 }}>
         <FlatList
           data={this.props.stations}
           keyExtractor={station => station.code}
           renderItem={station => (
-            <ListItem key={station.item.code} station={station.item} navigation={this.props.navigation} isFavorite={this.props.favorite.indexOf(station) > -1}/>
+            <ListItem
+              key={station.item.code}
+              station={station.item}
+              navigation={this.props.navigation}
+              isFavorite={this.props.favorite.indexOf(station) > -1}
+            />
           )}
         />
       </View>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => ({ stations: state.stations, favorite: state.favorite })
+const mapStateToProps = state => ({
+  stations: state.stations,
+  favorite: state.favorite
+});
 
-const ConnectedList = connect(mapStateToProps)(ListPage)
+const ConnectedList = connect(mapStateToProps)(ListPage);
 
-export default ConnectedList
+export default ConnectedList;
